092efc692e009ed0d1bea42807178feb8e5050a1
Anton Parkhomchuk <anton.parhom@gmail.com>
2023-12-17 05:58:04 +0200

    HW2

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { UserList2 } from './UserList2';
+import * as React from "react";
 
 export type AddressType = {
   street: string;
@@ -10,11 +11,11 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType; // пропиши типизацию
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: Array<UserType>; // пропиши типизацию
 };
 
 export const HW2 = () => {
@@ -47,7 +48,7 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    const filteredUsers = users.myFriends.filter(user => user.address.city === 'Los Angeles');
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
